07915ce817791a1c3ef4914c36b9f8f4d324ff51
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-12-10 12:51:12 +0100

    coments upd

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,9 @@
-// SCENE SETTINGS
+
+/* 
+####################
+## SCENE SETTINGS ##
+####################
+*/
 // ground params
 const GROUND_WIDTH = 600;
 const GROUND_HEIGHT = 400;
@@ -10,7 +15,11 @@ const CAMERA_ZOOM = 500;
 const CAMERA_POS_X = 300;
 const CAMERA_POS_Y = 200;
 
-// SYSTEM SETTINGS
+/* 
+#######################
+### SYSTEM SETTINGS ###
+#######################
+*/
 // agent amount
 var AMOUNT_AGENTS = 100;
 // Promień sąsiedztwa
