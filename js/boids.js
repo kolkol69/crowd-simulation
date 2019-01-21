765d861b87344497a22dd02c5e1bbe8db0bb2df6
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2019-01-21 18:48:32 +0100

    boids bartk algo upd

--- js/boids.js
+++ js/boids.js
@@ -1,12 +1,10 @@
 var boids;
 var agents = new Array(agentsAmount);
-var globalScene;
-var globalGrid;
 
 const width = GROUND_WIDTH;
 const height = GROUND_HEIGHT;
 
-// calculate new speed and direction
+//calculate new speed and direction
 function modify_speed_and_direction() {
 	let dist = 0.0;
 	let deg = 0.0;
@@ -17,7 +15,7 @@ function modify_speed_and_direction() {
 		boids[i].mean_d = 0;
 		boids[i].num = 1;
 		for (j = 0; j < agentsAmount; j++) {
-			if (j === i) continue;
+			if (j == i) continue;
 			dist = Math.sqrt(Math.pow(boids[i].x - boids[j].x, 2) + Math.pow(boids[i].y - boids[j].y, 2));
 			deg = Math.acos(
 				boids[i].vx / Math.sqrt(boids[i].vx * boids[i].vx + boids[i].vy * boids[i].vy) *
@@ -45,7 +43,7 @@ function modify_speed_and_direction() {
 
 		if (boids[i].num > 1) boids[i].mean_d /= (boids[i].num - 1);
 		for (j = 0; j < agentsAmount; j++) {
-			if (j === i) continue;
+			if (j == i) continue;
 			dist = Math.sqrt(Math.pow(boids[i].x - boids[j].x, 2) + Math.pow(boids[i].y - boids[j].y, 2));
 			deg = Math.acos(
 				boids[i].vx / Math.sqrt(boids[i].vx * boids[i].vx + boids[i].vy * boids[i].vy) *
@@ -80,7 +78,7 @@ function modify_speed_and_direction() {
 	}
 }
 
-// move and display boids
+//move and display boids
 function move_and_display() {
 	//first modify speed and direction
 	modify_speed_and_direction();
@@ -94,23 +92,34 @@ function move_and_display() {
 			boids[i].vy = (weightPerturbation * ((Math.random() - 0.5) * maxVelocity));
 		}
 
-		// move boid
+		//move boid
 		boids[i].x += boids[i].vx;
 		boids[i].y += boids[i].vy;
 
-		// check if outside window
+		//check if outside window
 		if (boids[i].x > width) boids[i].x -= width;
 		else if (boids[i].x < 0) boids[i].x += width;
 		if (boids[i].y > height) boids[i].y -= height;
 		else if (boids[i].y < 0) boids[i].y += height;
 
-		// display new position of boid
+		//display new position of boid
 		if (boids[i].vx < 0) {
 			boids[i].rotate = 90.0 + Math.atan(boids[i].vy / boids[i].vx) * 180.0 / Math.PI;
 		} else {
 			boids[i].rotate = -90.0 + Math.atan(boids[i].vy / boids[i].vx) * 180.0 / Math.PI;
 		}
 
+
+		let area = isWithinTargetArea(boids[i].x, boids[i].y)
+		if (area != -1) {
+			/*
+			            if (~~(Math.random() * chanceToLeaveTarget) == 0) {
+			                boids[i].recently_visited_target_id = area;
+			            }*/
+			boids[i].recently_visited_target_id = area;
+			//console.log("odwiedzony:" + area);
+			boids[i].preferences[area] = 1;
+		}
 	}
 
 	setBoidsPosition(boids);
@@ -119,43 +128,29 @@ function move_and_display() {
 
 const isHittingObstacle = (boid_next_pos_x, boid_next_pos_y) => {
 	for (let i = 0; i < OBSTACLE_POSITIONS.length; i++) {
-		if ((boid_next_pos_x + WIDTH > OBSTACLE_POSITIONS[i].x - OBSTACLE_POSITIONS[i].width / 2 && boid_next_pos_x - WIDTH < OBSTACLE_POSITIONS[i].x + OBSTACLE_POSITIONS[i].width / 2) &&
-			(boid_next_pos_y + WIDTH > OBSTACLE_POSITIONS[i].y - OBSTACLE_POSITIONS[i].depth / 2 && boid_next_pos_y - WIDTH < OBSTACLE_POSITIONS[i].y + OBSTACLE_POSITIONS[i].depth / 2)) {
+		if ((boid_next_pos_x >= OBSTACLE_POSITIONS[i].x - OBSTACLE_POSITIONS[i].width / 2 && boid_next_pos_x <= OBSTACLE_POSITIONS[i].x + OBSTACLE_POSITIONS[i].width / 2) &&
+			(boid_next_pos_y >= OBSTACLE_POSITIONS[i].y - OBSTACLE_POSITIONS[i].depth / 2 && boid_next_pos_y <= OBSTACLE_POSITIONS[i].y + OBSTACLE_POSITIONS[i].depth / 2)) {
 			return true;
 		}
 	}
 	return false;
 }
 
-const isBoidInsideSquare = () => {
-	for (let i = 0; i < boids.length; i++) {
-
-		dance: for (let j = 0; j < globalGrid.length; j++) {
-			if (boids[i].x >= globalGrid[j].position.x && boids[i].x <= globalGrid[j].position.x + SQUARE_SIZE && boids[i].y >= globalGrid[i].position.z && boids[i].y <= globalGrid[j].position.z + SQUARE_SIZE) {
-				let myMaterial = new BABYLON.StandardMaterial("myMaterial", globalScene);
-
-				let r = globalGrid[j].material.diffuseColor.r;
-				r = r + 0.005;
-				myMaterial.diffuseColor = new BABYLON.Color3(r, 0, 0);
-				globalGrid[j].material = myMaterial;
-				break dance;
-			}
+const isWithinTargetArea = (boid_x, boid_y) => {
+	for (let i = 0; i < TARGET_POSITIONS.length; i++) {
+		if (TARGET_POSITIONS[i].depth == 0) {
+			let dist = getDistance(boid_x, TARGET_POSITIONS[i].x, boid_y, TARGET_POSITIONS[i].y);
+			if (dist < width) return i;
+		}
+		if ((boid_x >= TARGET_POSITIONS[i].x - TARGET_POSITIONS[i].width / 2 && boid_x <= TARGET_POSITIONS[i].x + TARGET_POSITIONS[i].width / 2) &&
+			(boid_y >= TARGET_POSITIONS[i].y - TARGET_POSITIONS[i].depth / 2 && boid_y <= TARGET_POSITIONS[i].y + TARGET_POSITIONS[i].depth / 2)) {
+			return i;
 		}
 	}
-}
-
-const fadeSquare = () => {
-	for (let j = 0; j < globalGrid.length; j++) {
-		let myMaterial = new BABYLON.StandardMaterial("myMaterial1", globalScene);
-		let r = globalGrid[j].material.diffuseColor.r;
-		r = r - 0.003;
-		myMaterial.diffuseColor = new BABYLON.Color3(r, 0, 0);
-		globalGrid[j].material = myMaterial;
-	}
+	return -1;
 }
 
 const setBoidsPosition = (boids) => {
-
 	if (agentsAmount > agents.length) {
 		createMoreMeshes(agents);
 	}
@@ -163,74 +158,129 @@ const setBoidsPosition = (boids) => {
 		agents[i].position.x = boids[i].x;
 		agents[i].position.z = boids[i].y;
 	}
-
-
 }
 
 const getDistance = (x1, x2, y1, y2) => {
-	return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+	return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
-const setBoidsTargets = (boids) => {
-	for (i = 0; i < agentsAmount; i++) {
-		boids[i].current_target_id = getNextTarget(boids[i], i);
+const getNumberOfBoidsInArea = (index) =>{
+	let number = 0;
+	for(i = 0; i < agents.length; i++){
+		if (TARGET_POSITIONS[index].depth == 0) {
+			let dist = getDistance(boids[i].x, TARGET_POSITIONS[index].x, boids[i].y, TARGET_POSITIONS[index].y);
+			if (dist < width) number = number +1;
+		}
+		if ((boids[i].x >= TARGET_POSITIONS[index].x - TARGET_POSITIONS[index].width / 2 && boids[i].x <= TARGET_POSITIONS[index].x + TARGET_POSITIONS[index].width / 2) &&
+			(boids[i].y >= TARGET_POSITIONS[index].y - TARGET_POSITIONS[index].depth / 2 && boids[i].y <= TARGET_POSITIONS[index].y + TARGET_POSITIONS[index].depth / 2)) {
+			number = number + 1;
+		}
+	}
+	return number;
+}
 
+
+const setBoidsTargets = (boids) => {
+	for (let i = 0; i < agentsAmount; i++) {
+
+		/*
 		if (Math.floor(Math.random() * chanceToGetToTarget) == 0) { // for 3 there is 33.3% chance that agent will move towards defined target
-			if (boids[i].current_target_id == -1) {
+			// console.log(i % Math.floor(Math.random() * 2) + 2)
+			// const diagonal = Math.sqrt(Math.pow((boids[i].x - TARGET_POSITIONS[0].x), 2) + Math.pow((boids[i].y - TARGET_POSITIONS[0].y), 2)); //d^2 = (x0-xt)^2 + (y0-yt)^2 => d = sqrt((x0-xt)^2 + (y0-yt)^2)
+			const targetId = ~~(Math.random() * TARGET_POSITIONS.length);
+			const xDirection = boids[i].x - TARGET_POSITIONS[targetId].x > 0 ? -1 : 1;
+			const yDirection = boids[i].y - TARGET_POSITIONS[targetId].y > 0 ? -1 : 1;
+			// if (Math.pow(boids[i].x, 2) + Math.pow(boids[i].y, 2) - diagonal != 100) {
+			boids[i].vx = xDirection * (Math.random() * speedToTarget); // maxVelocity
+			boids[i].vy = yDirection * (Math.random() * speedToTarget);
+			// }
+            */
+
+		//console.log(isWithinTargetArea(boids[i].x, boids[i].y));
+
+		//let prev_target = boids[i].current_target_id;
+
+		
+		boids[i].current_target_id = getNextTarget(boids[i]);
+
+		//if (prev_target != boids[i].current_target_id) console.log("target changed: " + prev_target +" to " +  boids[i].current_target_id);
+		//else console.log("target not changed" + boids[i].current_target_id);
+		//boids[i].current_target_id = getNextTarget(boids[i].x, boids[i].y, boids[i].recently_visited_target_id, boids[i].current_target_id);
+		// console.log(boids[i].preferences);
+		if (boids[i].current_target_id == -1) {
+			// console.log("bez celu");
+			const myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+			myMaterial.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
+			agents[i].material = myMaterial;
+			if (Math.floor(Math.random() * chanceToGetToTarget) == 0) { // for 3 there is 33.3% chance that agent will move towards defined target
+				// console.log(i % Math.floor(Math.random() * 2) + 2)
+				// const diagonal = Math.sqrt(Math.pow((boids[i].x - TARGET_POSITIONS[0].x), 2) + Math.pow((boids[i].y - TARGET_POSITIONS[0].y), 2)); //d^2 = (x0-xt)^2 + (y0-yt)^2 => d = sqrt((x0-xt)^2 + (y0-yt)^2)
+				//const targetId = ~~(Math.random() * TARGET_POSITIONS.length);
 				let targetId;
 				do {
 					targetId = ~~(Math.random() * TARGET_POSITIONS.length);
-				} while (boids[i].recently_visited_target_id.indexOf(targetId) !== -1);
-				setBoidSpeed(boids[i], targetId);
-			} else {
-				// console.log('id:', boids[i].current_target_id, '>>>> ', TARGET_POSITIONS[boids[i].current_target_id]);
-				setBoidSpeed(boids[i], boids[i].current_target_id);
+				} while (targetId == boids[i].recently_visited_target_id);
+				const xDirection = boids[i].x - TARGET_POSITIONS[targetId].x > 0 ? -1 : 1;
+				const yDirection = boids[i].y - TARGET_POSITIONS[targetId].y > 0 ? -1 : 1;
+
+
+
+				// if (Math.pow(boids[i].x, 2) + Math.pow(boids[i].y, 2) - diagonal != 100) {
+				boids[i].vx = xDirection * (Math.random() * speedToTarget); // maxVelocity
+				boids[i].vy = yDirection * (Math.random() * speedToTarget);
+				// }
+			}
+		} else {
+			const myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+			myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
+			agents[i].material = myMaterial;
+
+			// console.log(boids[i].current_target_id);
+			const xDirection = boids[i].x - TARGET_POSITIONS[boids[i].current_target_id].x > 0 ? -1 : 1;
+			const yDirection = boids[i].y - TARGET_POSITIONS[boids[i].current_target_id].y > 0 ? -1 : 1;
+			// if (Math.pow(boids[i].x, 2) + Math.pow(boids[i].y, 2) - diagonal != 100) {
+			boids[i].vx = xDirection * (Math.random() * speedToTarget); // maxVelocity
+			boids[i].vy = yDirection * (Math.random() * speedToTarget);
+			// }
+
+		}
+	}
+}
+
+
+
+const getNextTarget = (boid) => {
+	let crowdThreshold = 50;
+	//gets first proper target to compare others to
+	let startingIndex = 0;
+	while (getDistance(boid.x, TARGET_POSITIONS[startingIndex].x, boid.y, TARGET_POSITIONS[startingIndex].y) > TARGET_POSITIONS[startingIndex].attraction_range ||
+		boid.preferences[startingIndex] == 1 || getNumberOfBoidsInArea(startingIndex) >= crowdThreshold) {
+		startingIndex = startingIndex + 1;
+		if (startingIndex == TARGET_POSITIONS.length) return -1;
+	}
+
+	let targetId = startingIndex;
+	let currdist = getDistance(boid.x, TARGET_POSITIONS[targetId].x, boid.y, TARGET_POSITIONS[targetId].y);
+	if (targetId == boid.current_target_id) currdist = currdist * currentTargetPriority;
+	currdist = currdist * boid.preferences[j] * ( 0.5 + (getNumberOfBoidsInArea(startingIndex) /(2 * crowdThreshold)));
+	for (let j = startingIndex + 1; j < TARGET_POSITIONS.lenght; j++) {
+		if (boid.preferences[startingIndex] < 1) {
+			let crowd = getNumberOfBoidsInArea(j);
+			let dist = getDistance(boid.x, TARGET_POSITIONS[targetId].x, boid.y, TARGET_POSITIONS[targetId].y);
+			if (dist <= TARGET_POSITIONS[j].attraction_range || crowd < crowdThreshold ) {
+				if (j == boid.current_target_id) dist = dist * currentTargetPriority;
+				dist = dist * boid.preferences[j] * (0.5 + (crowd /(2 * crowdThreshold)));
+				if (dist < currdist) {
+					currdist = dist;
+					targetId = j;
+				}
 			}
 		}
 	}
+	return targetId;
 }
 
-const setBoidSpeed = (boid, id) => {
-	const xDirection = boid.x - TARGET_POSITIONS[id].x > 0 ? -1 : 1;
-	const yDirection = boid.y - TARGET_POSITIONS[id].y > 0 ? -1 : 1;
 
-	boid.vx = xDirection * (Math.random() * speedToTarget); // maxVelocity
-	boid.vy = yDirection * (Math.random() * speedToTarget);
-}
-
-const getNextTarget = (boid, index) => {
-	// console.log('Visited places: ', ...boid.recently_visited_target_id);
-
-	const amountOfTargets = TARGET_POSITIONS.length;
-	// FINISH
-	if (boid.recently_visited_target_id.length === amountOfTargets) {
-		console.log('%c >>> ALL TARGETS ACHIEVED <<<', 'background: #222; color: #b70037');
-		agents[index].material.diffuseColor = new BABYLON.Color3(1, 0, 0);
-		boid.recently_visited_target_id = [];
-		return -1;
-	}
-
-	const targetDistances = TARGET_POSITIONS.map(_target => ~~getDistance(boid.x, _target.x, boid.y, _target.y) - _target.attraction_range);
-
-	let targetDistancesFiltered = TARGET_POSITIONS.map((_target, index) => {
-		if (boid.recently_visited_target_id.indexOf(index) === -1) {
-			return ~~getDistance(boid.x, _target.x, boid.y, _target.y) - _target.attraction_range;
-		}
-	}).filter(el => typeof el !== 'undefined');
-
-	targetID = targetDistances.indexOf(Math.min(...targetDistancesFiltered));
-
-	const distanceToTarget = getDistance(boid.x, TARGET_POSITIONS[targetID].x, boid.y, TARGET_POSITIONS[targetID].y) - TARGET_POSITIONS[targetID].attraction_range;
-	const isMovingToTarget = distanceToTarget < 0;
-	const isNotAlreadyVisited = boid.recently_visited_target_id.indexOf(targetID) === -1;
-
-	// Target achived and can be added to "visited"
-	if (isMovingToTarget && isNotAlreadyVisited) {
-		boid.recently_visited_target_id.push(targetID);
-	}
-
-	return targetID;
-}
 
 function createMoreMeshes(agents) {
 	for (let i = agents.length - 1; i < agentsAmount - 1; i++) {
@@ -243,13 +293,15 @@ function createMoreMeshes(agents) {
 	}
 }
 
+
+
 //initialize data
 const init = (scene) => {
 	agents = createAgentMeshes(scene);
 	boids = new Array(agentsAmount);
-	let vx;
-	let vy;
 	for (i = 0; i < agentsAmount; i++) {
+		let vx;
+		let vy;
 		boids[i] = {};
 		boids[i].x = Math.floor(Math.random() * width);
 		boids[i].y = Math.floor(Math.random() * height);
@@ -260,8 +312,32 @@ const init = (scene) => {
 		boids[i].vx = vx;
 		boids[i].vy = vy;
 
-		boids[i].recently_visited_target_id = [];
-		boids[i].current_target_id = ~~(Math.random() * TARGET_POSITIONS.length);
+		boids[i].recently_visited_target_id = -1;
+		/*
+		let targetId = 0;//~~(Math.random() * TARGET_POSITIONS.length);
+		let x1 = boids[i].x;
+		let y1 = boids[i].y;
+		let x2 = TARGET_POSITIONS[0].x;
+		let y2 = TARGET_POSITIONS[0].y;
+		let currdist = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+		for (let j = 1; j < TARGET_POSITIONS.lenght; j++) {
+		    x1 = boids[i].x;
+		    y1 = boids[i].y;
+		    x2 = TARGET_POSITIONS[j].x;
+		    y2 = TARGET_POSITIONS[j].y;
+		    let dist = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+		    if (dist < currdist) {
+		        currdist = dist;
+		        targetId = j;
+		    }
+		}
+		boids[i].current_target_id = targetId;
+		*/
+		boids[i].current_target_id = ~~(Math.random() * (TARGET_POSITIONS.length + 1)) - 1;
+		boids[i].preferences = new Array(TARGET_POSITIONS.length).fill(Math.random());
+		//console.log(boids[i].current_target_id);
+		//let dist = getDistance(boids[i].x, boids[i].y, TARGET_POSITIONS[0].x, TARGET_POSITIONS[0].y);
+		//if (dist < 200) { boids[i].current_target_id = 0; }
 
 	}
 	intervalID = setInterval(() => {
@@ -269,11 +345,7 @@ const init = (scene) => {
 	}, timeToTarget);
 }
 
-function start(scene, grid) {
-	globalScene = scene;
-	globalGrid = grid;
-	setInterval(isBoidInsideSquare, 500);
-	setInterval(fadeSquare, 4000);
+function start() {
 	init();
 	move_and_display();
 }
